5404369604a8ca92b4c683cbfbb79c24146eee59
niangdev03 <niangdev031299@gmail.com>
2025-08-20 16:18:52 +0000

    Redefinition de DBUtil

--- src/main/java/com/soap/Utils/DBUtil.java
+++ src/main/java/com/soap/Utils/DBUtil.java
@@ -1,31 +1,51 @@
 package com.soap.Utils;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
+import java.sql.SQLException;
 
 public class DBUtil {
-    private static String url;
-    private static String user;
-    private static String password;
+
+    // Configuration PostgreSQL
+    private static String url = "jdbc:postgresql://localhost:5432/niang-devoir-soap";
+    private static String username = "postgres"; 
+    private static String password = "root";
 
     static {
-        try (InputStream input = DBUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
-
-            url = prop.getProperty("db.url");
-            user = prop.getProperty("db.username");
-            password = prop.getProperty("db.password");
-
-            Class.forName(prop.getProperty("db.driver"));
-        } catch (Exception e) {
+        try {
+            // Driver PostgreSQL
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(url, user, password);
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
     }
 }
+
+// public class DBUtil {
+//     private static String url;
+//     private static String user;
+//     private static String password;
+
+//     static {
+//         try (InputStream input = DBUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
+//             Properties prop = new Properties();
+//             prop.load(input);
+
+//             url = prop.getProperty("db.url");
+//             user = prop.getProperty("db.username");
+//             password = prop.getProperty("db.password");
+
+//             Class.forName(prop.getProperty("db.driver"));
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+
+//     public static Connection getConnection() throws Exception {
+//         return DriverManager.getConnection(url, user, password);
+//     }
+// }
